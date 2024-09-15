4a40cdb75b9da7b7fb91e1da9f07a4e9efbf4927
Sirojiddin Tohirov <sir.tohirov@newuu.uz>
2024-09-15 15:08:03 +0500

    Good or bad

--- problem3.cpp
+++ problem3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    double c=0;
+    double c=;
     cin>>c;
     double f=c*9/5+32;
     cout<<"The temperature in Fahrenheit is equal to "<<f<<endl;
